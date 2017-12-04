5331dc6914f887f95a2ff13751782e936534b27b
devyangss <devyangss@163.com>
2017-12-04 12:26:29 +0800

    update

--- Helpers/StringHelper.php
+++ Helpers/StringHelper.php
@@ -4,5 +4,19 @@ namespace php\helpers;
 
 class StringHelper extends BaseStringHelper
 {
-    
+
+    /**
+     * 中文字符串长度
+     * @param string $string
+     */
+    public static function chineseStrlen($string = '')
+    {
+		$ret = 0;
+		if (empty($string)) {
+			return $ret;
+		}
+        preg_match_all("/([\x{4e00}-\x{9fa5}])/u", $string, $match);
+        
+        return count($match[0]);
+	}
 }
